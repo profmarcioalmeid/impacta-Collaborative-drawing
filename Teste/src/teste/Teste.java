69f1e0183e172ca3643804e310dc148b008530a0
profmarcioalmeid <prof.marcioalmeid@gmail.com>
2015-03-11 17:14:33 -0300

    Teste2

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -15,6 +15,6 @@ public class Teste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Teste");
+        System.out.println("Teste 2015");
     }
 }
