824f89df148812d86da7b6a151c10501d509581b
prof marcio <prof.marcioalmeid@gmail.com>
2015-11-04 15:24:51 -0200

    organizando projetos

--- Collaborative-drawing-Client/src/ws/quadro/QuadroClient.java
+++ Collaborative-drawing-Client/src/ws/quadro/QuadroClient.java
@@ -3,21 +3,28 @@ package ws.quadro;
 import java.net.MalformedURLException;
 import java.net.URL;
  
+
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 
 public class QuadroClient {
-	public static void main(String[] args) throws MalformedURLException{
+	public static void main(String[] args) throws MalformedURLException, InterruptedException{
 		
-		URL url = new URL("http://127.0.0.1:54321/quadro?wsdl");
+		URL url = new URL("http://10.0.200.116:54321/quadro?wsdl");
 		QName qname = new QName("http://quadro.ws/","QuadroImplService");
 		
 		Service service = Service.create(url, qname);
 		Quadro quadro =  service.getPort(Quadro.class);
-                       quadro.setDesenho("<circle id=\"1\" cx=\"100\" cy=\"200\" r=\"10\" stroke=\"Black\" stroke-width=\"10\" fill=\"#00FFFF\"  />");
+			
+						for(int i=10; i < 100; i++){
+							//Thread.sleep(500l);
+							quadro.setRemetente("José ");
+							quadro.setDesenho("<circle id=\"1\" cx=\""+i+"\" cy=\"350\" r=\"10\" stroke=\"Black\" stroke-width=\"10\" fill=\"#00FFFF\"  />");
+							System.out.println(quadro.getDesenho());
+						}
 		
-		System.out.println(quadro.getDesenho());
+		
 		 
 		
 	}
