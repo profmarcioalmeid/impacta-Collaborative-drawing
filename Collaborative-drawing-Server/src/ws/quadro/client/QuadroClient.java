b33be2b41313b690e2906fab869223722342e5aa
prof marcio <prof.marcioalmeid@gmail.com>
2015-11-04 15:25:15 -0200

    organizando projetos

--- Collaborative-drawing-Server/src/ws/quadro/client/QuadroClient.java
+++ Collaborative-drawing-Server/src/ws/quadro/client/QuadroClient.java
@@ -12,17 +12,18 @@ import ws.quadro.Quadro;
 public class QuadroClient {
 	public static void main(String[] args) throws MalformedURLException{
 		
-		URL url = new URL("http://127.0.0.1:54321/quadro?wsdl");
+		URL url = new URL("http://10.0.200.116:54321/quadro?wsdl");
 		QName qname = new QName("http://quadro.ws/","QuadroImplService");
 		
 		Service service = Service.create(url, qname);
 		Quadro quadro =  service.getPort(Quadro.class);
                  
-            //    quadro.setRemetente("TESTE");
-               // for(int i=10; i <= 100; i++ ){                    
-                  //  quadro.setDesenho("<circle id=\"1\" cx=\""+i+"\" cy=\"200\" r=\"10\" stroke=\"Black\" stroke-width=\"10\" fill=\"#00FFFF\"  />");
-               // }
-                String desenho = quadro.getDesenho();
+                quadro.setRemetente("TESTE");
+                for(int i=10; i <= 100; i++ ){                    
+                    quadro.setDesenho("<circle id=\"1\" cx=\""+i+"\" cy=\"300\" r=\"10\" stroke=\"Black\" stroke-width=\"10\" fill=\"#00FFFF\"  />");
+               }
+                
+              /*  String desenho = quadro.getDesenho();
                 System.err.println(" Remetente "+ quadro.getRemetente());
 		for(;;)	
                 {
@@ -31,7 +32,7 @@ public class QuadroClient {
                     System.out.println(quadro.getDesenho());
                     desenho=quadro.getDesenho();
                     }
-                }
+                }*/
 		 
 		
 	}
