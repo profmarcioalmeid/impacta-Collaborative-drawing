b138bcbcce478a6c9d871e4b7c9417f75d4f5298
prof marcio <prof.marcioalmeid@gmail.com>
2015-12-01 15:27:15 -0200

    bugs corrections

--- Collaborative-drawing-Client/src/ws/quadro/Quadro.java
+++ Collaborative-drawing-Client/src/ws/quadro/Quadro.java
@@ -6,10 +6,10 @@ import javax.jws.WebService;
 @WebService
 public interface Quadro {
     
-        @WebMethod void setRemetente(String id);
+    @WebMethod void setRemetente(String id);
 	@WebMethod void setDesenho(String svg);
 	@WebMethod String getDesenho(); 
-        @WebMethod String getRemetente(); 
+    @WebMethod String getRemetente(); 
 	@WebMethod String test();
 	
 }
--- Collaborative-drawing-Client/src/ws/quadro/QuadroClient.java
+++ Collaborative-drawing-Client/src/ws/quadro/QuadroClient.java
@@ -1,33 +1,20 @@
 package ws.quadro;
-
 import java.net.MalformedURLException;
 import java.net.URL;
- 
-
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
-
 public class QuadroClient {
-	public static void main(String[] args) throws MalformedURLException, InterruptedException{
-		
-		URL url = new URL("http://10.0.200.116:54321/quadro?wsdl");
-		QName qname = new QName("http://quadro.ws/","QuadroImplService");
-		
+	public static void main(String[] args) throws MalformedURLException, InterruptedException{		
+		URL url = new URL("http://10.0.200.243:54321/quadro?wsdl");
+		QName qname = new QName("http://quadro.ws/","QuadroImplService");		
 		Service service = Service.create(url, qname);
-		Quadro quadro =  service.getPort(Quadro.class);
-			
+		Quadro quadro =  service.getPort(Quadro.class);			
 						for(int i=10; i < 100; i++){
 							//Thread.sleep(500l);
-							quadro.setRemetente("José ");
+							quadro.setRemetente("Josï¿½ ");
 							quadro.setDesenho("<circle id=\"1\" cx=\""+i+"\" cy=\"350\" r=\"10\" stroke=\"Black\" stroke-width=\"10\" fill=\"#00FFFF\"  />");
 							System.out.println(quadro.getDesenho());
-						}
-		
-		
-		 
-		
+						}		
 	}
- 
-
 }
